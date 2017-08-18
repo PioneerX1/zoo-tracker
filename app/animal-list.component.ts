c4e34ca8aeb360c3ba71d86394d258a0779e0191
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-18 14:47:44 -0700

    add age filter pipe

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,9 +4,14 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals" selected="selected">All Animals</option>
+    <option value="young">Young Animals Under 2 Years</option>
+    <option value="mature">Mature Animals</option>
+  </select>
 
   <ul class="list-group">
-    <li class="list-group-item" *ngFor="let currentAnimal of childAnimalList">
+    <li class="list-group-item" *ngFor="let currentAnimal of childAnimalList | age: filterByAge">
       <h4>{{currentAnimal.name}}   <em>| {{currentAnimal.species}}</em></h4>
       <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit Animal</button>
     </li>
@@ -20,5 +25,11 @@ export class AnimalListComponent {
 
   editButtonHasBeenClicked(animalToEdit: Animal) {
     this.clickSender.emit(animalToEdit);
+  }
+
+  filterByAge: string = "allAnimals";
+
+  onChange(optionFromMenu) {
+    this.filterByAge = optionFromMenu;
   }
 }
